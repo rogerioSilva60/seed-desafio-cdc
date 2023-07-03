2cceff8c94fa1c90b1da31bcf22fd00bd4235686
rogerio <rogeriosilva60@gmail.com>
2023-07-03 18:54:54 -0300

    Adicionado projeto inicial

---
+++ src/main/java/com/deveficiente/lojadelivro/LojadelivroApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.lojadelivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadelivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadelivroApplication.class, args);
+	}
+
+}
